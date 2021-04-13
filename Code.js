b717d4ec7b674fb8bd0f2609a54ba4d15b1356b9
Dakdak <yandatson@gmail.com>
2021-04-12 21:40:46 -0400

    Revised version

--- Code.js
+++ Code.js
@@ -1,3 +1,29 @@
-function myFunction() {
-  
-}
+"use strict";
+
+const allSheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+const links = [
+  "firstLink",
+  "secondLink",
+  "thirdLink",
+  "fourthLink",
+  "fifthLink",
+  "sixthLink",
+];
+
+const fillCells = function (sheetPosition, link1, link2, link3) {
+  sheetPosition.getRange("A3").setValue(link1);
+  sheetPosition.getRange("B3").setValue(link2);
+  sheetPosition.getRange("C3").setValue(link3);
+};
+
+const fillAllSheets = function (allSheets) {
+  let index = 0;
+  allSheets.forEach((sheet) => {
+      fillCells(sheet, links[index], links[++index], links[++index]);
+      index++;
+  });
+};
+
+function execute() {
+  fillAllSheets(allSheets);
+};
